9034794db3fdc4d1a277c4b8014aed2dc0ca1889
Caspian <caspian.root@gmail.com>
2025-07-15 21:27:15 -0500

    HEMOS TERMINADO DE CREAR LA FUNCON DE DATOS, LA FUNCION DE VALIDAR DATOS FUNCIONA BIEN.

--- Modulo1-PeriodosDePrueba.py
+++ Modulo1-PeriodosDePrueba.py
@@ -7,6 +7,7 @@ error = Fore.RED + Style.BRIGHT
 exito = Fore.GREEN + Style.BRIGHT
 blanco = Fore.WHITE + Style.BRIGHT
 
+contratos_validos = ("indeterminado", "inicio o incremento de actividades", "necesidades del mercado", "reconversion empresarial", "ocasional", "suplencia", "emergencia", "especifico", "intermitente", "temporada")
 def clear():
     return os.system("cls" if os.name == 'nt' else "clear")
 
@@ -30,7 +31,7 @@ def validacion_nombre():
 def validacion_cargo():
     while True:
         clear()
-        print("PARA CANCELAR INGRESE 'CANCELAR'. ")
+        print("PARA CANCELAR INGRESE 'cancelar'. ")
         try:
             print("CARGO:")
             cargo = input(">>> ").lower().strip()
@@ -44,6 +45,23 @@ def validacion_cargo():
         except ValueError:
             input("CARGO NO VALIDO")
 
+def validacion_contrato():
+    while True:
+        clear()
+        print("PARA CANCELAR INGRESE 'cancelar'. ")
+        try:
+            print("CONTRATO:")
+            contrato = input(">>> ").lower().strip()
+            if contrato in ("cancelar", "salir"):
+                return None, False
+            else:
+                if len(contrato.split()) <= 5 and all(palabra.isalpha() for palabra in contrato) and contrato in contratos_validos:
+                    return contrato, True
+                else:
+                    raise ValueError
+        except ValueError:
+            input("CONTRATO NO ENCONTRADO")
+
 def datos():
 
     nombre, estado_nombre = validacion_nombre()
@@ -55,10 +73,11 @@ def datos():
     if not estado_cargo:
         input("CANCELADO")
         return
-
-        
-
-
+    
+    contrato, estado_contrato = validacion_contrato()
+    if not estado_contrato:
+        input("CANCELADO")
+        return
 
 def main():
     datos()
